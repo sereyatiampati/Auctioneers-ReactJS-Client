fef3ad6fd7665f7bdb918b4109ce019dc6d79c2e
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-04-17 11:57:26 +0300

    added the footer.jsx code and the footer.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+//import Signup from './components/signup/Signup'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        
+        <Footer/>
     </div>
   )
 }
