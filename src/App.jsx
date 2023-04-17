a9cd5c2e7545bbad389af9f0f771d0695422240c
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-04-17 14:15:37 +0300

    updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Home from './components/home/Home'
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <Home/>
+      <Navbar />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
