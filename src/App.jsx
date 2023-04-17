e54c7b263f67774a2ae71fb05e9f8107b0a95af3
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-17 09:13:17 +0300

    Removed banner from App.js after testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import { Banner } from './components/banner/Banner'
 import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
-        <Banner/>
     </div>
   )
 }
