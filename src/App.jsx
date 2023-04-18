7467f5f427deb73b7caaf2b7e86482932825af3c
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-04-18 23:40:54 +0300

    added route on my App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Bidhistory from './components/bidhistory/Bidhistory';
 import Signup from './components/signup/Signup';
-import Home from './components/home/Home';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
        <Navbar/>
         <Routes>
           <Route path='/signup' element={<Signup/>}/>
-          <Route path='/' element={<Home/>}/>
           <Route path='/bids' element={<Bidhistory/>} />
         </Routes>
     </div>
