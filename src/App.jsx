6488b0a5cbe2cb363f6717bc444ffa1ea3ddbf14
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-04-18 23:48:13 +0300

    added route on my App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import './App.css';
+import Home from './components/home/Home';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Bidhistory from './components/bidhistory/Bidhistory';
 import Signup from './components/signup/Signup';
@@ -6,14 +9,14 @@ import Signup from './components/signup/Signup';
 function App() {
   return (
     <div className="body">
-    <Navbar/>
-        <Routes>
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/auctions' />
-          <Route path='/vendors' />
-          <Route path='/contact' />
-          </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/' element={<Home/> }/>
+        <Route path='/auctions' />
+        <Route path='/vendors' />
+        <Route path='/contact' />
+      </Routes>
     </div>
   );
 }
