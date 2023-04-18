7f570ea57d00ce21faf7a905c3c81a727b41f1f6
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-04-18 12:26:18 +0000

    complete NotFound and Login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+
 import './App.css'
-import Navbar from './components/navbar/Navbar'
-import LoginForm from './components/login-form/login-form'
+import { Routes, Route } from "react-router-dom"
+import Login from './pages/login/login'
+import NotFound from './pages/404/NotFound'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const value = [loggedIn, setLoggedIn];
   return (
-    <div className="App">
-        <LoginForm/>
-    </div>
+    <>
+        <Routes>
+          <Route path="/login" element={<Login value={value}/>} />
+          <Route path="*" element={<NotFound value={value}/>} />
+        </Routes>
+    </>
   );
 }
 export default App;
