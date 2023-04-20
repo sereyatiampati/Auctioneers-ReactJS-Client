cace6378ee69181bdd8eaf7a7fdb0f006dd31abd
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-04-20 05:13:17 +0000

    Add seller portal page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Login from './pages/login/login'
 import NotFound from './pages/404/NotFound'
 import BidPage from './pages/bid/BidPage'
+import Seller from './pages/seller-page/seller'
 import Navbar from './components/navbar/Navbar';
 import Signup from './components/signup/Signup';
 import Home from './components/home/Home'
@@ -18,11 +19,12 @@ function App() {
         <Routes>
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/' element={<Home/>}/>
-          <Route path='/product-bid' element={<BidPage/>}/>
           <Route path='/auctions' />
           <Route path='/vendors' />
           <Route path='/contact' />
           <Route path="/login" element={<Login value={value}/>} />
+          <Route path='/product-bid' element={<BidPage/>}/>
+          <Route path='/seller' element={<Seller/>}/>
           <Route path="*" element={<NotFound value={value}/>} />
         </Routes>
     </div>
