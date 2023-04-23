724765d8e9987611ac4803e2064d36927daa49c4
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-23 22:34:02 +0300

    Implemented countdown logic in card.jsx

--- src/components/ProductCard/Card.jsx
+++ src/components/ProductCard/Card.jsx
@@ -1,39 +1,53 @@
-import { useNavigate } from "react-router";
-import BidPage from "../bidPage/BidPage";
-function Card({product}) {
-    const {id,name, image} = product
-    const navigate= useNavigate()
+import React, { useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
+import './card.css'
+function Card({ product }) {
+    const [timeRemaining, setTimeRemaining] = useState('');
+    const navigate = useNavigate()
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const now = new Date();
+            const startDate = product && product.start_date ? new Date(product.start_date) : null;
+            const endDate = product && product.end_date ? new Date(product.end_date) : null;
+            const timeDiff = endDate && startDate ? endDate.getTime() - now.getTime() : 0;
+            const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+            const minutes = Math.floor((timeDiff / (1000 * 60)) % 60);
+            const seconds = Math.floor((timeDiff / 1000) % 60);
+            if (!endDate || timeDiff <= 0) {
+                setTimeRemaining('Auction Ended');
+            } else {
+                setTimeRemaining(`Auction ending in: ${days} day(s) ${hours} hour(s) ${minutes} minute(s) ${seconds} second(s)`);
+            }
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, [product]);
+    console.log(product.name);
     return (
-    <div class="product-card col-md-4 mt-2" >   
-        <div class="card">
-                            <div class="card-body">
-                                <div class="card-img-actions">
-                                        <img src={image} class="card-img img-fluid" alt="" style={{height: "240px", width: '300px', objectFit: 'cover'}} onClick={()=>navigate(`/auction/${id}`)}/>
-                                </div>
-                            </div>
-
-                            <div class="card-body pb-1">
-                                <div class="mb-2">
-                                    <h6 class="font-weight-semibold mb-2">
-                                        <a class="text-dark mb-2" data-abc="true">{name}</a>
-                                    </h6>
-
-                                    <p style={{color: "#4A60A1"}} data-abc="true">Top Bid: 36,000 KES</p>
-                                </div>
-
-                                <p class="text-muted" data-abc="true">6 bids placed</p>
-
-                                <div class="text-muted mb-3">Condition: No visible defects</div>
-                                <div class="text-muted mb-2 pb-2 border-bottom" >Auction ending in: 1 day 6 hours</div>
-
-                                <button type="button" class="btn bg-light mr-3" style={{color: "#4A60A1"}}><i class="bi bi-share"></i></button>
-                                <button type="button" class="btn bg-light mr-3" style={{color: "#4A60A1"}}><i class="fa-regular fa-heart"></i></button>
-                                <button type="button" class="btn bg-light" style={{color: "#4A60A1"}} onClick={()=>navigate(`/auction/${id}`)}><i class="fa-solid fa-plus"></i> Bid</button>
-                            </div>
-                        </div>        
-
-   </div>
-     );
+        <div class="col-md-4 mt-2">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-img-actions">
+                        <img src={product.image} class="card-img img-fluid" width="96" height="300px" alt="" />
+                    </div>
+                </div>
+                <div class="card-body pb-1">
+                    <div class="mb-2">
+                        <h6 class="font-weight-semibold mb-2">
+                            <a href="#" class="text-dark mb-2" data-abc="true">{product.name}</a>
+                        </h6>
+                        <p style={{ color: "#4A60A1" }} data-abc="true">Start price : ${product.starting_price}</p>
+                    </div>
+                    <p class="text-muted" data-abc="true">Start Date : {new Date(product.start_date).toLocaleString()}</p>
+                    <div class="text-muted mb-3"> End Date: {new Date(product.end_date).toLocaleString()}</div>
+                    <div class="text-muted mb-2 pb-2 border-bottom" >{timeRemaining}</div>
+                    <button disabled={timeRemaining === 'Auction Ended'} type="button" class="btn bg-light mr-3" style={{ color: "#4A60A1" }}><i class="bi bi-share"></i></button>
+                    <button disabled={timeRemaining === 'Auction Ended'} type="button" class="btn bg-light mr-3" style={{ color: "#4A60A1" }}><i class="fa-regular fa-heart"></i></button>
+                    <button disabled={timeRemaining === 'Auction Ended'} type="button" class="btn bg-light" style={{color: "#4A60A1"}} onClick={()=>navigate(`/auction/${id}`)}><i class="fa-solid fa-plus"></i> Bid</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
