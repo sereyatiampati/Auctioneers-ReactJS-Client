3520ec9dba61e9ce2f0253fdf3914779bb0b8d7e
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-23 20:33:27 +0300

    Added styling to dynamic cards

--- src/components/ProductCard/Carding.jsx
+++ src/components/ProductCard/Carding.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './card.css'
 function Card({ product }) {
     const [timeRemaining, setTimeRemaining] = useState('');
     useEffect(() => {
