8b529307620d2f0a108fe64336c9c58fe9817ed5
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-27 00:31:42 +0300

    Implemented filter functionality

--- src/components/bidderpage/BidderPage.jsx
+++ src/components/bidderpage/BidderPage.jsx
@@ -8,8 +8,9 @@ import API_BASE_URL from "../../utilities/env";
 function BidderPage() {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [bidsPerPage, setBidsPerPage] = useState(9);
-  const [sortingOption, setSortingOption] = useState('all');
+  const [bidsPerPage, setbidsPerPage] = useState(9);
+  const [filter, setFilter] = useState("all");
+
   useEffect(() => {
     fetch(`${API_BASE_URL}/products`)
       .then(res => res.json())
@@ -20,39 +21,46 @@ function BidderPage() {
       .catch(error => console.log(error));
   }, []);
 
-  const handleSortingOptionChange = (event) => {
-    setSortingOption(event.target.value);
-  }
-
-  console.log('products:', products);
-  const filteredProducts = sortingOption === 'open' ? products.filter(p => p.status === 'open') : products;
-  const sortedProducts = [...filteredProducts].sort((a, b) => {
-    if (sortingOption === 'open') {
-      return new Date(a.end_time) - new Date(b.end_time);
-    } else {
-      return b.bid_count - a.bid_count;
+  const now = new Date();
+  const filteredProducts = products.filter((product) => {
+    if (filter === "all") {
+      return true;
+    }
+    if (filter === "active") {
+      if (!product.end_date) {
+        return true;
+      }
+      const endDate = new Date(product.end_date);
+      return endDate > now;
+    }
+    if (filter === "ended") {
+      if (!product.end_date) {
+        return false;
+      }
+      const endDate = new Date(product.end_date);
+      return endDate <= now;
     }
   });
-  console.log(sortedProducts);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   const indexOfLastBid = currentPage * bidsPerPage;
   const indexOfFirstBid = indexOfLastBid - bidsPerPage;
-  const currentBids = sortedProducts.slice(indexOfFirstBid, indexOfLastBid);
+  const currentBids = Array.isArray(products) ? filteredProducts.slice(indexOfFirstBid, indexOfLastBid) : [];
+
   return (
     <>
-    <Banner />
-    <div className="text-center"> 
-        <div className="sorting">
-          <label htmlFor="sorting-option">Sort By:</label>
-          <select id="sorting-option" value={sortingOption} onChange={handleSortingOptionChange}>
-            <option value="open">Open Bids</option>
-            <option value="all">All Bids</option>
-          </select>
-        </div>
-      </div>
-      
-      <ProductsContainer products={currentBids} />
-      <Pagination totalbids={sortedProducts.length} bidsPerPage={bidsPerPage} setCurrentPage={setCurrentPage} currentPage={currentPage} />
       <Banner />
+      <div className="filter">
+        <label htmlFor="filter-select">Filter by:</label>
+        <select id="filter-select" value={filter} onChange={handleFilterChange}>
+          <option value="all">All auctions</option>
+          <option value="active">Active auctions</option>
+          <option value="ended">Ended auctions</option>
+        </select>
+      </div>
       <ProductsContainer products={currentBids} />
       <Pagination totalbids={products.length} bidsPerPage={bidsPerPage} setCurrentPage={setCurrentPage} currentPage={currentPage} />
     </>
