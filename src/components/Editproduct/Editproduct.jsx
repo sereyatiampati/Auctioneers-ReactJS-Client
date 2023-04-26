71a526301888a2d7b279d50fd410f07b757428e9
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-26 20:52:18 +0300

    modified:   src/components/Editproduct/Editproduct.jsx

--- src/components/Editproduct/Editproduct.jsx
+++ src/components/Editproduct/Editproduct.jsx
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router';
 import { useParams } from 'react-router-dom';
 import React from 'react';
 
-export default function Editproduct ()  {
+export default function Editproduct() {
     // Define the list of categories
     const [categories, setCategories] = useState([])
-    const [product,setProduct]=useState([])
+    const [product, setProduct] = useState([])
     // For the form
     const [name, setName] = useState('');
     const [category, setCategory] = useState(null);
@@ -19,7 +19,7 @@ export default function Editproduct ()  {
     const { id } = useParams();
     const navigate = useNavigate();
 
-    
+
 
     useEffect(() => {
         fetch('http://localhost:3000/categories')
@@ -29,42 +29,55 @@ export default function Editproduct ()  {
 
     useEffect(() => {
         fetch(`http://localhost:3000/products/${id}`)
-        .then(res => res.json())
-        .then(data => setProduct(data))
-    },[id])
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, [id])
+
     const handleUpdate = (e) => {
         e.preventDefault();
-        
+
         const updatedProduct = {
-          name: name,
-          category_id: category.id,
-          start_time: startTime,
-          end_time: endTime,
-          starting_price: startingBid,
-          description: description,
-          image_url: imageUrl
+            name: name,
+            category_id: category.id,
+            start_time: startTime,
+            end_time: endTime,
+            starting_price: startingBid,
+            description: description,
+            image_url: imageUrl
         };
-      
+
         fetch(`http://localhost:3000/products/${id}`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(updatedProduct)
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedProduct)
         })
-          .then(response => {
-            if (response.ok) {
-              // navigate to the product details page for the updated product
-              navigate(`/seller`);
-            } else {
-              throw new Error('Failed to update product');
-            }
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      };
-      
+            .then(response => {
+                if (response.ok) {
+                    // navigate to the product details page for the updated product
+                    navigate(`/seller`);
+                } else {
+                    throw new Error('Failed to update product');
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    //   <label htmlfor="category">Category:</label>
+    //       <select id="categories" name="category_id" onChange={handleFormInput}>
+    //         <option value={editData?.category?.id}>
+    //           {produuct.category}
+    //         </option>
+    //         {categories.map((option) => (
+    //           <option key={option.id} value={option.id}>
+    //             {option.name}
+    //           </option>
+    //         ))}
+    //       </select>
+
     return (
         <div className='product-form'>
             <form onSubmit={handleUpdate}>
@@ -72,18 +85,19 @@ export default function Editproduct ()  {
                 <div className="row mb-4">
                     <div className="col">
                         <div className="form-outline">
-                            <label className="form-label" htmlFor="form6Example1">Product<span style={{color: 'red'}}>*</span></label>
+                            <label className="form-label" htmlFor="form6Example1">Product<span style={{ color: 'red' }}>*</span></label>
                             <input type="text" id="form6Example1" defaultValue={product.name} className="form-control" onChange={(e) => setName(e.target.value)} />
                         </div>
                     </div>
                     <div className="col">
                         <div className="form-outline">
                             <label className="form-label" htmlFor="category">Category<span style={{ color: 'red' }}>*</span></label>
-                            <select id="category" className="form-control"  onChange={(e) => setCategory({id : e.target.value})}>
-                                <option value="">Select a category</option>
-                                {categories.map(category => (
+                            <select id="category" className="form-control" onChange={(e) => setCategory({ id: e.target.value })}>
+                                <option value=''>Select a category</option>
+                                {categories.map(category => {
                                     <option key={category.id} value={category.id}>{category.category_name}</option>
-                                ))}
+
+                                })}
                             </select>
                         </div>
                     </div>
@@ -105,8 +119,8 @@ export default function Editproduct ()  {
                 <div className="row mb-4">
                     <div className="col">
                         <div className="form-outline">
-                            <label className="form-label" htmlFor="form6Example1">Starting Bid (KES)<span style={{color: 'red'}}>*</span></label>
-                            <input type="number" id="form6Example1" defaultValue={product.starting_price} className="form-control"onChange={(e) => setStartingBid(e.target.value)} />
+                            <label className="form-label" htmlFor="form6Example1">Starting Bid (KES)<span style={{ color: 'red' }}>*</span></label>
+                            <input type="number" id="form6Example1" defaultValue={product.starting_price} className="form-control" onChange={(e) => setStartingBid(e.target.value)} />
                         </div>
                     </div>
                 </div>
@@ -114,7 +128,7 @@ export default function Editproduct ()  {
                     <div className="col">
                         <div className="form-outline">
                             <label className="form-label" htmlFor="form6Example1">Product Description<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" id="form6Example1"defaultValue={product.description} className="form-control" onChange={(e) => setDescription(e.target.value)} />
+                            <input type="text" id="form6Example1" defaultValue={product.description} className="form-control" onChange={(e) => setDescription(e.target.value)} />
                         </div>
                     </div>
                 </div>
@@ -127,9 +141,6 @@ export default function Editproduct ()  {
                     </div>
                 </div>
                 <div className="row mb-4">
-                    {/* <div className="col">
-                        <button type="submit" className="btn btn-primary btn-block mb-4">Picture</button>
-                    </div> */}
                     <div className="col">
                         <button type="submit" className="btn btn-primary btn-block mb-4">Save</button>
                     </div>
